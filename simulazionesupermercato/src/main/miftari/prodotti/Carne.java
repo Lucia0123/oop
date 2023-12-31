383ccbaff0926d13a2b47d1f83530de8ddfe77a3
HiraMiftari <147724169+HiraMiftari@users.noreply.github.com>
2023-12-31 08:07:36 +0100

    Update Carne.java

--- simulazionesupermercato/src/main/miftari/prodotti/Carne.java
+++ simulazionesupermercato/src/main/miftari/prodotti/Carne.java
@@ -5,16 +5,17 @@ import java.util.Optional;
 
 public class Carne extends ProdottoAlimentare{
 
-         private TipoCarne tipo;
+         private TipoCarne tipoCarne;
 
             // Costruttore che chiama il costruttore della classe base e imposta dati specifici della carne
-            public Carne (double prezzo, LocalDate dataScadenza, TipoCarne tipo) {
+            public Carne (double prezzo, LocalDate dataScadenza, TipoCarne tipoCarne) {
                 super(prezzo, Optional.ofNullable(dataScadenza));
-                this.tipo = tipo;
+                
+  this.tipoCarne = tipoCarne;
             }
 
             // Metodo per ottenere il tipo di carne
             public TipoCarne getTipoCarne() {
-                return this.tipo;
+                return this.tipoCarne;
             }
 }
